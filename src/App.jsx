befe861e8c8c4fa603edc42a00058cda94387fcc
Javlonbk <javlonbeabr41@gmail.com>
2024-06-25 10:30:48 +0500

    useTransition

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
-import { Child } from './child'
-import { ErrorBoundary } from './components/error-boundry'
+import { useState, useTransition } from "react";
+import Cover from "./components/cover";
+import Reviews from "./components/reviews";
+import Writer from "./components/writer";
+import { StyledButton } from "./components/styled-elements";
 
-const App = () => {
-  return (  
-    <div>
-      <h1>Parent</h1>
-      <ErrorBoundary fallback={<h1 style={{color:"red"}}>Error in Child Component+</h1>}>
-      <Child/>
-      </ErrorBoundary>
-    </div>
-  )
+function App() {
+  const [section, setSection] = useState("Cover");
+
+  const sectionHandler = (sec) => {
+    setSection(sec);
+  };
+  return (
+    <>
+      <Button onClick={() => sectionHandler("Cover")}>Cover</Button>
+      <Button onClick={() => sectionHandler("Reviews")}>Book Reviews</Button>
+      <Button onClick={() => sectionHandler("Writer")}>Book's Writer</Button>
+
+      {section === "Cover" ? (
+        <Cover />
+      ) : section === "Reviews" ? (
+        <Reviews />
+      ) : (
+        <Writer />
+      )}
+    </>
+  );
 }
 
-export default App
+const Button = ({ onClick, ...props }) => {
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <StyledButton
+      onClick={() => {
+        startTransition(() => {
+          onClick();
+        });
+      }}
+      {...props}
+    />
+  );
+};
+
+export default App;
